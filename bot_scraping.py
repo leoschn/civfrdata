578394f55d5f639bfae1d042d59834717caf56c6
leoschn <133115659+leoschn@users.noreply.github.com>
2025-06-26 23:59:32 +0200

    change : rework database (team and player by discord id and team by current role)

--- bot_scraping.py
+++ bot_scraping.py
@@ -16,13 +16,7 @@ script_directory = path_list[0:len(path_list)-1]
 base_path =  "/".join(script_directory) + "/"
 
 
-def add_new_tables_to_db(db_file):
-    # Connexion à la base de données source (contenant la table "games")
-    conn = sqlite3.connect(db_file)
-
-
-#test
-def extract_from_string_raw(s, player_id_dict, role_id_dict, verbose=False):
+def extract_from_string_raw(s, verbose=False):
     data = {}
     pattern_role = r'\<@&(.*?)\>'
     pattern_user = r'\<@(.*?)\>'
@@ -44,18 +38,19 @@ def extract_from_string_raw(s, player_id_dict, role_id_dict, verbose=False):
     if 'vs' in splited_s[0+dec]:
 
         try :
-            data['Team A'] = role_id_dict[int(re.findall(pattern_role, splited_s[0+dec].split('vs')[0])[0])]
+            data['Team A'] = int(re.findall(pattern_role, splited_s[0+dec].split('vs')[0])[0])
         except:
             data['Team A']  = 'UNKNOWN'
 
+
         try :
-            data['Team B'] = role_id_dict[int(re.findall(pattern_role, splited_s[0+dec].split('vs')[1])[0])]
+            data['Team B'] = int(re.findall(pattern_role, splited_s[0+dec].split('vs')[1])[0])
         except:
             data['Team B'] = 'UNKNOWN'
 
         #extract winner
         try :
-            data['Winner'] = role_id_dict[int(re.findall(pattern_role, splited_s[1+dec])[0])]
+            data['Winner'] = int(re.findall(pattern_role, splited_s[1+dec])[0])
         except:
             data['Winner'] ='UNKNOWN'
 
@@ -114,7 +109,7 @@ def extract_from_string_raw(s, player_id_dict, role_id_dict, verbose=False):
                 try :
                     data['Ban{0}'.format(i+1)]=bans[i].strip()
                 except :
-                    data['Ban{0}'.format(i+1)]='UNKNOWN'
+                    data['Ban{0}'.format(i+1)]=0
 
 
             #extract leaders picks
@@ -131,11 +126,11 @@ def extract_from_string_raw(s, player_id_dict, role_id_dict, verbose=False):
             #extract player
         for i in range(4):
             try :
-                data['PlayerA{0}'.format(i+1)]=player_id_dict[int(re.findall(pattern_user, splited_s[6+i+dec])[0])]
+                data['PlayerA{0}'.format(i+1)]=int(re.findall(pattern_user, splited_s[6+i+dec])[0])
             except:
                 data['PlayerA{0}'.format(i+1)] = 'UNKNOWN'
             try:
-                data['PlayerB{0}'.format(i+1)] =player_id_dict[int(re.findall(pattern_user, splited_s[11+i+dec])[0])]
+                data['PlayerB{0}'.format(i+1)] =int(re.findall(pattern_user, splited_s[11+i+dec])[0])
             except:
                 data['PlayerB{0}'.format(i+1)] = 'UNKNOWN'
 
@@ -146,11 +141,11 @@ def extract_from_string_raw(s, player_id_dict, role_id_dict, verbose=False):
         raise 'Matching failed'
     return data
 
-def extract_from_serie_raw(s, player_id_dict, role_id_dict, verbose=False):
+def extract_from_serie_raw(s, verbose=False):
     l=[]
     for row in s.iterrows():
         try :
-            data = extract_from_string_raw(row[1]['message'], player_id_dict, role_id_dict, verbose)
+            data = extract_from_string_raw(row[1]['message'], verbose)
             data['Date'] = row[1]['date'].strftime("%d/%m/%y")
         except :
 
@@ -180,54 +175,58 @@ async def on_ready():
                 f'{guild.name}\n'
             )
             # Building user database
-            player_id_dict = {}
+            player_id_map = {}
             for member in guild.members:
                 id = member.id
                 name = member.display_name
-                player_id_dict[id]=name
+                role_list = member.roles
+                role_list = [role.id for role in role_list]
+                player_id_map[id]={}
+                player_id_map[id]["name"] = name
+                player_id_map[id]["role_list"] = role_list
 
         #Building role database (team)
-            role_id_dict = {}
+            role_id_map = {}
             for role in guild.roles:
                 id = role.id
                 name = role.name
-                role_id_dict[id]=name
+                role_id_map[id]=name
 
 
             c_channel = discord.utils.get(guild.text_channels, name='s15-reporting-d1')
             messages = [{'message':message.content,'date' : message.created_at} async for message in c_channel.history(after=datetime.datetime(2025,1,17,14,30),limit=1000)]
             df1 = pd.DataFrame(messages)
-            df1 = extract_from_serie_raw(df1, player_id_dict, role_id_dict)
+            df1 = extract_from_serie_raw(df1)
             df1['Division'] = '1'
 
             c_channel = discord.utils.get(guild.text_channels, name='s15-reporting-d2')
             messages = [{'message':message.content,'date' : message.created_at}  async for message in c_channel.history(after=datetime.datetime(2025,1,17,14,30),limit=1000)]
             df2 = pd.DataFrame(messages)
-            df2 = extract_from_serie_raw(df2, player_id_dict, role_id_dict)
+            df2 = extract_from_serie_raw(df2)
             df2['Division'] = '2'
 
             c_channel = discord.utils.get(guild.text_channels, name='s15-reporting-d3a')
             messages = [{'message':message.content,'date' : message.created_at}  async for message in c_channel.history(after=datetime.datetime(2025,1,17,14,30),limit=1000)]
             df3a= pd.DataFrame(messages)
-            df3a = extract_from_serie_raw(df3a, player_id_dict, role_id_dict)
+            df3a = extract_from_serie_raw(df3a)
             df3a['Division'] = '3a'
 
             c_channel = discord.utils.get(guild.text_channels, name='s15-reporting-d3b')
             messages = [{'message':message.content,'date' : message.created_at}  async for message in c_channel.history(after=datetime.datetime(2025,1,17,14,30),limit=1000)]
             df3b = pd.DataFrame(messages)
-            df3b = extract_from_serie_raw(df3b, player_id_dict, role_id_dict)
+            df3b = extract_from_serie_raw(df3b)
             df3b['Division'] = '3b'
 
             df = pd.concat([df1, df2, df3a, df3b], axis=0)
-            df.to_csv(base_path + 'data_S15.csv', index=False)
+            df.to_csv(base_path + 'data_S15_test.csv', index=False)
 
     print('report scrapped')
 
-    conn = sqlite3.connect(base_path + 'database.db')
+    conn = sqlite3.connect(base_path + 'database_test.db')
 
     conn.execute("DROP TABLE IF EXISTS games")
 
-    data = pd.read_csv(base_path + 'data_S15.csv')
+    data = pd.read_csv(base_path + 'data_S15_test.csv')
     data['id'] = data.index
     data.to_sql('games', conn)
 
@@ -241,75 +240,83 @@ async def on_ready():
     games_data = cursor.fetchall()
 
     # Dictionnaire pour stocker les joueurs uniques.
-    # Clé : pseudo du joueur.
+    # Clé : id du joueur.
     # Valeur : dictionnaire contenant :
-    #   - "teams" : dictionnaire avec comme clé le nom de l'équipe et comme valeur le nombre de matchs joués pour cette équipe.
-    #   - "games" : ensemble des IDs des matchs où il apparaît.
+    #   - "teams"       : dictionnaire avec comme clé l'ID de l'équipe et comme valeur le nombre de matchs joués pour cette équipe.
+    #   - "games"       : ensemble des IDs des matchs où il apparaît.
+    #   - "pseudo"      : player pseudo
+    #   - "current_team": current player team based on its discord role
     players_dict = {}
 
     # Dictionnaire pour stocker les équipes.
     # Clé : nom de l'équipe.
     # Valeur : dictionnaire contenant :
-    #   - "players" : ensemble des pseudos appartenant à l'équipe.
+    #   - "players" : ensemble des IDs appartenant à l'équipe.
     #   - "games"   : ensemble des IDs des matchs où l'équipe a joué.
+    #   - "name"    : team name
     #   - "division": la division de l'équipe (prise lors de la première occurrence).
     teams_dict = {}
 
     for row in games_data:
+
         game_id = row["index"]
-        division = row["Division"].strip() if row["Division"] else ""
-        teamA = row["Team A"].strip() if row["Team A"] else ""
-        teamB = row["Team B"].strip() if row["Team B"] else ""
+        division = row["Division"]
+        teamA = row["Team A"]
+        teamB = row["Team B"]
 
         # Mise à jour des informations pour Team A et Team B dans teams_dict
-        if teamA:
+        if teamA != 'UNKNOWN':
+            teamA = int(teamA)
             if teamA not in teams_dict:
                 teams_dict[teamA] = {"players": set(), "games": set(), "division": division}
             teams_dict[teamA]["games"].add(str(game_id))
-        if teamB:
+        if teamB != 'UNKNOWN':
+            teamB = int(teamB)
             if teamB not in teams_dict:
                 teams_dict[teamB] = {"players": set(), "games": set(), "division": division}
             teams_dict[teamB]["games"].add(str(game_id))
 
         # Pour les joueurs de l'équipe A (PlayerA1 à PlayerA4)
         for col in ["PlayerA1", "PlayerA2", "PlayerA3", "PlayerA4"]:
-            pseudo = row[col]
-            if pseudo and pseudo.strip():
-                pseudo = pseudo.strip()
-                if pseudo not in players_dict:
-                    players_dict[pseudo] = {"teams": {}, "games": set()}
-                players_dict[pseudo]["games"].add(str(game_id))
+            try :
+                id = int(row[col])
+                if id not in players_dict:
+                    players_dict[id] = {"teams": {}, "games": set(),'pseudo': player_id_map[id]['name']}
+                players_dict[id]["games"].add(str(game_id))
                 if teamA:
                     # Incrémente le compteur pour teamA
-                    players_dict[pseudo]["teams"][teamA] = players_dict[pseudo]["teams"].get(teamA, 0) + 1
+                    players_dict[id]["teams"][int(teamA)] = players_dict[id]["teams"].get(int(teamA), 0) + 1
+            except :
+                pass
 
         # Pour les joueurs de l'équipe B (PlayerB1 à PlayerB4)
         for col in ["PlayerB1", "PlayerB2", "PlayerB3", "PlayerB4"]:
-            pseudo = row[col]
-            if pseudo and pseudo.strip():
-                pseudo = pseudo.strip()
-                if pseudo not in players_dict:
-                    players_dict[pseudo] = {"teams": {}, "games": set()}
-                players_dict[pseudo]["games"].add(str(game_id))
+            try :
+                id = int(row[col])
+                if id not in players_dict:
+                    players_dict[id] = {"teams": {}, "games": set(),'pseudo': player_id_map[id]['name']}
+                players_dict[id]["games"].add(str(game_id))
                 if teamB:
                     # Incrémente le compteur pour teamB
-                    players_dict[pseudo]["teams"][teamB] = players_dict[pseudo]["teams"].get(teamB, 0) + 1
+                    players_dict[id]["teams"][int(teamB)] = players_dict[id]["teams"].get(int(teamB), 0) + 1
+            except :
+                pass
 
-    # Pour chaque joueur, déterminer l'équipe pour laquelle il a joué le plus de matchs
-    for pseudo, info in players_dict.items():
-        if info["teams"]:
-            best_team = max(info["teams"].items(), key=lambda x: x[1])[0]
-            info["team"] = best_team
-        else:
-            info["team"] = ""
+    # Pour chaque joueur, déterminer son équipe actuelle
+    for id, info in players_dict.items():
+        info["current_team"] = 'NONE'
+        for team_id in info['teams'].keys() :
+            if team_id in player_id_map[id]['role_list']:
+                info["current_team"] = team_id
+
 
     # Compléter teams_dict avec la liste des joueurs extraits
-    for pseudo, info in players_dict.items():
-        team = info["team"]
-        if team:
+    for id, info in players_dict.items():
+        team = info["current_team"]
+        if team is not 'NONE':
             if team not in teams_dict:
                 teams_dict[team] = {"players": set(), "games": set(), "division": ""}
-            teams_dict[team]["players"].add(pseudo)
+            teams_dict[team]["players"].add(id)
 
     # Suppression des tables existantes si elles existent déjà
     cursor.execute("DROP TABLE IF EXISTS players")
@@ -321,8 +328,8 @@ async def on_ready():
     # Création de la table players (pour les joueurs)
     cursor.execute('''
             CREATE TABLE players (
-                player_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                pseudo TEXT NOT NULL,
+                player_id INTEGER,
+                player_name TEXT NOT NULL,
                 team TEXT NOT NULL
             )
         ''')
@@ -339,7 +346,8 @@ async def on_ready():
     # Création de la table teams (pour les équipes)
     cursor.execute('''
             CREATE TABLE teams (
-                team_name TEXT PRIMARY KEY,
+                team_id INTEGER PRIMARY KEY,
+                team_name TEXT,
                 division TEXT
             )
         ''')
@@ -347,9 +355,9 @@ async def on_ready():
     # Création de la table team_players (liaison équipe - joueur)
     cursor.execute('''
             CREATE TABLE team_players (
-                team_name TEXT,
+                team_id INTEGER,
                 player_id INTEGER,
-                FOREIGN KEY(team_name) REFERENCES teams(team_name),
+                FOREIGN KEY(team_id) REFERENCES teams(team_id),
                 FOREIGN KEY(player_id) REFERENCES players(player_id)
             )
         ''')
@@ -357,32 +365,30 @@ async def on_ready():
     # Création de la table team_games (liaison équipe - match)
     cursor.execute('''
             CREATE TABLE team_games (
-                team_name TEXT,
+                team_id INTEGER,
                 game_id INTEGER,
-                FOREIGN KEY(team_name) REFERENCES teams(team_name)
+                FOREIGN KEY(team_id) REFERENCES teams(team_id)
             )
         ''')
 
     # Insertion des joueurs dans la table players et création du mapping pseudo -> player_id
-    player_id_map = {}
-    for pseudo, info in players_dict.items():
-        team = info["team"]
-        cursor.execute("INSERT INTO players (pseudo, team) VALUES (?, ?)", (pseudo, team))
-        new_player_id = cursor.lastrowid
-        player_id_map[pseudo] = new_player_id
+    player_id_dict = {}
+    for id, info in players_dict.items():
+        team = info["current_team"]
+        cursor.execute("INSERT INTO players (player_id, player_name, team) VALUES (?,?, ?)", (id,player_id_map[id]['name'], team))
         for game_id in info["games"]:
-            cursor.execute("INSERT INTO player_games (player_id, game_id) VALUES (?, ?)", (new_player_id, int(game_id)))
+            cursor.execute("INSERT INTO player_games (player_id, game_id) VALUES (?, ?)", (id, int(game_id)))
 
     # Insertion des équipes dans la table teams et dans les tables de liaison
-    for team_name, info in teams_dict.items():
-        division = info.get("division", "")
-        cursor.execute("INSERT INTO teams (team_name, division) VALUES (?, ?)", (team_name, division))
-        for game_id in info["games"]:
-            cursor.execute("INSERT INTO team_games (team_name, game_id) VALUES (?, ?)", (team_name, int(game_id)))
-        for pseudo in info["players"]:
-            player_id = player_id_map.get(pseudo)
-            if player_id:
-                cursor.execute("INSERT INTO team_players (team_name, player_id) VALUES (?, ?)", (team_name, player_id))
+    for team_id, info in teams_dict.items():
+        if team_id!='UNKNOWN':
+            team_id = int(team_id)
+            division = info.get("division", "")
+            cursor.execute("INSERT INTO teams (team_id, team_name, division) VALUES (?, ?, ?)", (team_id, role_id_map[team_id], division))
+            for game_id in info["games"]:
+                cursor.execute("INSERT INTO team_games (team_id, game_id) VALUES (?, ?)", (team_id, int(game_id)))
+            for id in info["players"]:
+                cursor.execute("INSERT INTO team_players (team_id, player_id) VALUES (?, ?)", (team_id, id))
 
     conn.commit()
     conn.close()
