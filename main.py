b6ec5a0db6b21ff5c9ac8f51c9644326d8cb8093
leoschn <133115659+leoschn@users.noreply.github.com>
2025-09-21 14:35:34 +0200

    fix add inland EvsW map

--- main.py
+++ main.py
@@ -111,7 +111,8 @@ list_map_url = [
     ("primordial", 'Map_Primodial_29.webp', "Primordial"),
     ("inland sea", 'Map_Inland_Sea_29.webp', "Inland Sea"),
     ("pangaea est-west", 'Inland_Est_West-25322f67.png', "Pangaea Est-West "),
-    ("inland est-west","Inland_Est_West-25322f67.png","Inland Est-West")
+    ("inland est-west","Inland_Est_West-25322f67.png","Inland Est-West"),
+    ("inland sea est-west","Inland_Est_West-25322f67.png","Inland Est-West")
 ]
 
 # Dictionnaires pour les assets (chemin vers l'image)
